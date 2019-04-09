e5132b74c1d39cae907cbaf832b0e638c7a41e3a
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-04-09 17:37:25 +0900

    Add Skelton.

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+const path = require(`path`)
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/img/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from "react"
 import styles from "../styles/header.module.css"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default () => (
+export default ({ data }) => (
   <div className={styles.header}>
+    <div>{/* <Img fixed={data.file.childImageSharp.fixed} /> */}</div>
     <ul className={styles.lists}>
       <li className={styles.list}>
         <Link to="/profile/">Profile</Link>
@@ -17,3 +20,17 @@ export default () => (
     </ul>
   </div>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "blog/avatars/kyle-mathews.jpeg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
