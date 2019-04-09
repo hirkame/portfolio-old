411d57e6d6fe56627e747c83bf261db116903506
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-04-09 20:46:21 +0900

    Added a header logo and completed design tasks of header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,39 @@
 import React from "react"
 import styles from "../styles/header.module.css"
-import { Link } from "gatsby"
-import { graphql } from "gatsby"
+import { Link, graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
-export default ({ data }) => (
-  <div className={styles.header}>
-    <div>{/* <Img fixed={data.file.childImageSharp.fixed} /> */}</div>
-    <ul className={styles.lists}>
-      <li className={styles.list}>
-        <Link to="/profile/">Profile</Link>
-      </li>
-      <li className={styles.list}>
-        <Link to="/work/">Work</Link>
-      </li>
-      <li className={styles.list}>
-        <Link to="/posts/">Posts</Link>
-      </li>
-    </ul>
-  </div>
+export default () => (
+  <StaticQuery
+    query={query}
+    render={data => (
+      <div className={styles.header}>
+        <div className="container">
+          <div className={styles.logo}>
+            <Img fixed={data.file.childImageSharp.fixed} />
+          </div>
+          <ul className={styles.lists}>
+            <li className={styles.list}>
+              <Link to="/profile/">Profile</Link>
+            </li>
+            <li className={styles.list}>
+              <Link to="/work/">Work</Link>
+            </li>
+            <li className={styles.list}>
+              <Link to="/posts/">Posts</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )}
+  />
 )
 
-export const query = graphql`
+const query = graphql`
   query {
-    file(relativePath: { eq: "blog/avatars/kyle-mathews.jpeg" }) {
+    file(relativePath: { eq: "logo.png" }) {
       childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fixed(width: 125, height: 125) {
+        fixed(width: 90, height: 37) {
           ...GatsbyImageSharpFixed
         }
       }
