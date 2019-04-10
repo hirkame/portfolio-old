b04ed506462c07aa1b32a41740502e866e8740fd
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-04-10 17:49:24 +0900

    Complete desktop-design of home page and profile page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,56 @@
 import React from "react"
+import styles from "../styles/home.module.css"
+import { graphql, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
 import Container from "../components/container"
-import Introduction from "../components/introduction"
-import Passion from "../components/passion"
 import Header from "../components/header"
-import Footer from "../components/footer"
+
+// export default () => (
+//   <div>
+//     <Header />
+//     <h1>
+//       Hello! <br />I am Hiroki Kameyama from Japan.{" "}
+//     </h1>
+//     <Img
+//       fixed={data.file.childImageSharp.fluid}
+//       fadeIn={false}
+//       alt="mountain"
+//       style={{ position: "absolute" }}
+//     />
+//   </div>
+// )
 
 export default () => (
-  <div>
-    <Header />
-    <Container>
-      <Introduction />
-      <Passion />
-    </Container>
-    <Container>
-      <Footer />
-    </Container>
-  </div>
+  <StaticQuery
+    query={query}
+    render={data => (
+      <div>
+        <Header />
+        <div className={styles.indexContainer}>
+          <h1 className={styles.greeting}>
+            Hello! <br />I am Hiroki Kameyama from Japan.{" "}
+          </h1>
+          <Img
+            fluid={data.file.childImageSharp.fluid}
+            className={styles.mountain}
+            fadeIn={false}
+            alt="mountain"
+            style={{ position: "absolute" }}
+          />
+        </div>
+      </div>
+    )}
+  />
 )
+
+const query = graphql`
+  query {
+    file(relativePath: { eq: "mountain.png" }) {
+      childImageSharp {
+        fluid(quality: 100, maxWidth: 3000) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+  }
+`
