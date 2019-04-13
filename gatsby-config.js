2665aeb9e5ab85ba425f1a5f88e40c10eea748d6
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-04-13 16:42:11 +0900

    Integrated with Prismic. Will add the posts page for the next action.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,9 @@
 const path = require(`path`)
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   plugins: [
     {
@@ -7,6 +11,14 @@ module.exports = {
       options: {
         name: `img`,
         path: `${__dirname}/src/img/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `portfolio-posts`,
+        accessToken: `${process.env.API_KEY}`,
+        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
       },
     },
     `gatsby-plugin-sharp`,
---
+++ src/templates/post.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+const Post = ({ data: { prismicPost } }) => {
+  const { data } = prismicPost
+  return (
+    <React.Fragment>
+      <h1>{data.title.text}</h1>
+      <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
+    </React.Fragment>
+  )
+}
+
+export default Post
+
+export const pageQuery = graphql`
+  query PostBySlug($uid: String!) {
+    prismicPost(uid: { eq: $uid }) {
+      uid
+      data {
+        post_date
+        post_title {
+          html
+        }
+        post_summary {
+          html
+        }
+        post_cover {
+          alt
+          copyright
+          url
+        }
+        post_group {
+          post_image {
+            alt
+            copyright
+            url
+          }
+          post_content {
+            html
+            text
+          }
+        }
+      }
+    }
+  }
+`
