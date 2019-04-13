850c7968ae6df2d11ec781f42d0c67c9fa2a1e2a
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-04-13 14:40:22 +0900

    Delete unnecesary comments.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,7 @@ import React from "react"
 import styles from "../styles/home.module.css"
 import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
-import Container from "../components/container"
 import Header from "../components/header"
-
-// export default () => (
-//   <div>
-//     <Header />
-//     <h1>
-//       Hello! <br />I am Hiroki Kameyama from Japan.{" "}
-//     </h1>
-//     <Img
-//       fixed={data.file.childImageSharp.fluid}
-//       fadeIn={false}
-//       alt="mountain"
-//       style={{ position: "absolute" }}
-//     />
-//   </div>
-// )
 
 export default () => (
   <StaticQuery
