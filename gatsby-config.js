1b7f0d2e8ed980c571e1764b848d230a3020569a
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-05-03 18:10:34 +0900

    Added a plugin about Google Analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
   plugins: [
     { resolve: `gatsby-plugin-sitemap` },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-139557585-1",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
