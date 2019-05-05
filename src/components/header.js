fda59a0ac9d9c188814b03709f517e9de071d745
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-05-05 17:36:50 +0900

    Set up the header for mobile.

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ export default () => (
     render={data => (
       <div className={styles.header}>
         <div className="container">
-          <div className={styles.logo}>
+          <div className={styles.mobile_logo}>
             <Link to="/">
               <Img
                 fixed={data.file.childImageSharp.fixed}
@@ -36,6 +36,43 @@ export default () => (
               </Link>
             </li>
           </ul>
+          <div className={styles.mobile_nav}>
+            <label htmlFor="label1" className={styles.nomargin}>
+              <span className={styles.nav_icon} />
+            </label>
+            <input type="checkbox" id="label1" className={styles.input} />
+            <div className={styles.hidden_show}>
+              <div className="container">
+                <div className={styles.mobile_logo}>
+                  <Link to="/">
+                    <Img
+                      fixed={data.file.childImageSharp.fixed}
+                      critical={true}
+                      fadeIn={false}
+                      alt="logo"
+                    />
+                  </Link>
+                </div>
+                <ul className={styles.mobile_lists}>
+                  <li className={styles.mobile_list}>
+                    <Link className={styles.link} to="/">
+                      Home
+                    </Link>
+                  </li>
+                  <li className={styles.mobile_list}>
+                    <Link className={styles.link} to="/profile/">
+                      Profile
+                    </Link>
+                  </li>
+                  <li className={styles.mobile_list}>
+                    <Link className={styles.link} to="/posts/">
+                      Posts
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )}
