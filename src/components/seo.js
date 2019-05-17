10c8e238a8c1ecbf28625e34409fe7fa9bfb9b6e
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-05-18 02:18:53 +0900

    One more!

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-const SEO = ({ title, description, image, pathname, article }) => (
+const SEO = ({ title, description, image, article }) => (
   <StaticQuery
     query={query}
     render={({
